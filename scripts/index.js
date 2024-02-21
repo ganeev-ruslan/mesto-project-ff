a28eb01880f38399272014ac129efb5d75693e56
ganeev-ruslan <145503389+ganeev-ruslan@users.noreply.github.com>
2024-02-21 15:42:52 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(name, link) {
+    const cardTemplate = document.querySelector('#card-template');
+    const cardElement = cardTemplate.content.firstElementChild.cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
+    cardElement.querySelector('.card__title').textContent = name;
+
+    // Добавление обработчика события для кнопки удаления
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function() {
+      cardElement.remove();
+    });
+
+    // Добавление обработчика события для кнопки лайка
+    const likeButton = cardElement.querySelector('.card__like-button');
+    likeButton.addEventListener('click', function() {
+      likeButton.classList.toggle('card__like-button_active');
+    });
+
+    const placesList = document.querySelector('.places__list');
+    placesList.appendChild(cardElement);
+  }
+
+  // Создание карточек из массива initialCards
+  initialCards.forEach(function(card) {
+    createCard(card.name, card.link);
+  });
+
