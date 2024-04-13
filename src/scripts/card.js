ee0cc0ca753ffad419cde6f061fe5ed0d3bd2f40
ganeev-ruslan <145503389+ganeev-ruslan@users.noreply.github.com>
2024-04-14 01:17:08 +0300

    Update card.js

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,70 +1,37 @@
-import { likeCardApi, unlikedCard, deleteCardApi } from "./api";
+//Функция добавляет обработчики событий на элементы карточки (изображение, кнопки удаления и лайка) и применяет  изменения к карточке.
+//После всех манипуляций функция возвращает созданный элемент карточки.
 
-function createCard(cardData, userID, openImageCallback) {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  const cardImage = cardElement.querySelector(".card__image");
-  const buttonLike = cardElement.querySelector(".card__like-button");
-  const deleteButton = cardElement.querySelector(".card__delete-button");
+const createCard = ({ userIdIdentifier, sample, data, deleteServerCard, likeServerCard, imageClickUrl }) => {
+  const element = sample.querySelector('.card').cloneNode(true);
+  const likeCounter = element.querySelector('.card__like-counter');
+  const image = element.querySelector('.card__image');
+  const deleteButton = element.querySelector('.card__delete-button');
+  const likeButton = element.querySelector('.card__like-button');
 
-  cardImage.src = cardData.link;
-  cardImage.alt = cardData.name;
-  cardElement.querySelector(".card__title").textContent = cardData.name;
+  const { name, link, likes, owner, _id } = data;
 
-  deleteButton.addEventListener("click", () => deleteCard(cardElement, cardData._id));
-  buttonLike.addEventListener("click", () => likeCard(buttonLike, cardData._id, cardElement));
-  cardImage.addEventListener("click", () => openImageCallback(cardData));
-  
-  //условие проверяет, поставил ли текущий пользователь лайк на данную карточку.
+  image.addEventListener('click', () => imageClickUrl({ cardName: name, cardLink: link }));
+  image.src = link;
+  image.alt = name;
+  element.querySelector('.card__title').textContent = name;
 
-  if (cardData.likes.some((addlike)=> addlike._id ===userID)){
-    buttonLike.classList.add('card__like-button_is-active')
+  if (likes.length) {
+    likeCounter.classList.add('card__like-counter_is-active');
+    likeCounter.textContent = likes.length;
   }
 
-  countLike(cardElement, cardData);
-  return cardElement
-}
-
-//удаление карточки из пользовательского интерфейса и с сервера.
-function deleteCard(cardToRemoveElement, cardId) {
-  deleteCardApi(cardId)
-    .then((сardData) => {
-      cardToRemoveElement.remove();
-      console.log("Карточка успешно удалена:", сardData);
-    })
-    .catch((error) => {
-      console.log("Произошла ошибка при удалении карточки:", error);
-    });
-}
-//обработчик действия по нажатию на кнопку "лайк" карточки.
-function likeCard(addlike, cardId, elem) {
-  if (addlike.classList.contains("card__like-button_is-active")) {
-    unlikedCard(cardId)
-      .then((res) => {
-        addlike.classList.remove("card__like-button_is-active");
-        countLike(elem, res);
-      })
-      .catch((error) => {
-        console.log("Произошла ошибка при добавлении лайка:", error);
-      });
-  } else {
-    likeCardApi(cardId)
-      .then((res) => {
-        addlike.classList.add("card__like-button_is-active");
-        countLike(elem, res);
-      })
-      .catch((error) => {
-        console.log("Произошла ошибка при добавлении лайка:", error);
-      });
+  if (owner['_id'] === userIdIdentifier) {
+    deleteButton.classList.add('card__delete-button_is-active');
+    deleteButton.addEventListener('click', () => deleteServerCard({ cardId: _id, cardElement: element, buttonElement: deleteButton }));
   }
-}
 
-//обновляет отображение количества лайков в элементе с классом "count" на основе значения из объекта
-function countLike(elem, card) {
-  const count = elem.querySelector(".count");
-  if (count) {
-    count.textContent = card.likes.length;
+  if (likes.map(like => like['_id']).includes(userIdIdentifier)) {
+    likeButton.classList.add('card__like-button_is-active');
   }
-}
 
-export {createCard};
+  likeButton.addEventListener('click', () => likeServerCard({ cardId: _id, buttonElement: likeButton, counterElement: likeCounter }));
+
+  return element;
+};
+
+export { createCard };
